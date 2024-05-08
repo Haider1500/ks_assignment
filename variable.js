3de9b21a039aae388d5421dffb5f26d7dbd09a30
Ali Haider <ali1500haider@gmail.com>
2024-05-08 20:24:37 +0500

    added variable file

---
+++ variable.js
@@ -0,0 +1,21 @@
+var a = 10
+var a =17
+
+console.log(a);
+
+
+let b = 20
+b=12
+console.log(b)
+
+const c = 10
+console.log(c)
+
+{
+    let b = 20
+    const c = 49
+    console.log(c,)
+    console.log(b,"let inside scope")
+}
+console.log(b,"let outside scope")
+console.log(c,"const outside scope")
